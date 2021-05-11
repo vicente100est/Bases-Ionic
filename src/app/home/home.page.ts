ef3a777dcf14edac7847be207ae75b3d3c1e5132
Vicente Estrada <vicenteestradomin@gmail.com>
2021-05-10 21:30:57 -0500

    Bases Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  componentes: Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+  ];
 
   constructor() {}
 
